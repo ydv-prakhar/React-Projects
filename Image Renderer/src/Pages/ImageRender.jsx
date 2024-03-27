ca8ec6af272ab15e8d74c8f7c6de6e891de22e73
Prakhar Yadav <yadavprakhar1809@gmail.com>
2024-03-27 14:52:55 +0530

    feat:Fixed Bug

--- Image Renderer/src/Pages/ImageRender.jsx
+++ Image Renderer/src/Pages/ImageRender.jsx
@@ -6,50 +6,53 @@ const ImageRender = () => {
   const [page, setPage] = useState(1);
   const [images, setImages] = useState([]);
 
+
   useEffect(() => {
-    const initialize = async () => {
-      await fetchImages();
-    };
-    initialize(page);
-  }, []);
+    const fetchImages = async () => {
+      let config = {
+        method: "get",
+        maxBodyLength: Infinity,
+        url: `https://api.pexels.com/v1/search/?query=cats&page=${page}`,
+        headers: {
+          Authorization: import.meta.env.VITE_API_KEY,
+        },
+      };
 
-  const fetchImages = async (pageNumber) => {
-    let config = {
-      method: "get",
-      maxBodyLength: Infinity,
-      url: `https://api.pexels.com/v1/search/?query=dogs&page=${pageNumber}`,
-      headers: {
-        Authorization: import.meta.env.VITE_API_KEY,
-      },
-    };
-
-    try {
-      const response = await axios.request(config);
-      if (response.status == 200) {
-        const photos = response.data.photos.map((photo) => {
-          return photo.src.landscape;
-        });
-        setImages((prevImages) => [...prevImages, ...photos]);
-        setPage((prevPage) => prevPage + 1);
+      try {
+        const response = await axios.request(config);
+        if (response.status == 200) {
+          const photos = response.data.photos.map((photo) => {
+            return photo.src.landscape;
+          });
+          setImages((prevImages) => [...prevImages, ...photos]);
+        }
+      } catch (e) {
+        console.log(e);
+        return [];
       }
-    } catch (e) {
-      console.log(e);
-      return [];
-    }
-  };
+    };
+
+      fetchImages();
+
+    return () => {};
+  }, [page]);
 
   return (
     <InfiniteScroll
       dataLength={images?.length}
-      next={()=>fetchImages(page)}
+      next={() => {
+        setPage((prevPage) => prevPage + 1);
+      }}
       hasMore={true}
       loader={<h4>Loading...</h4>}
     >
-      {images?.map((i, index) => (
-        <div key={index}>
-          <img src={i} alt="" />
-        </div>
-      ))}
+      <div className="">
+        {images?.map((i, index) => (
+          <div className="flex justify-center my-5" key={index}>
+            <img src={i} alt="" />
+          </div>
+        ))}
+      </div>
     </InfiniteScroll>
   );
 };
--- Image Renderer/src/main.jsx
+++ Image Renderer/src/main.jsx
@@ -1,10 +1,5 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import ImageRender from './Pages/ImageRender'
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import ImageRender from "./Pages/ImageRender";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <ImageRender/>
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(<ImageRender />);

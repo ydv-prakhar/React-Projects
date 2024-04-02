401989d2c79d93d0a4382d96ea5855ab7dc80d96
Prakhar Yadav <yadavprakhar1809@gmail.com>
2024-04-02 17:44:42 +0530

    Redux Setup

---
+++ Redux Practice Project/src/store.js
@@ -0,0 +1,8 @@
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+
+import taskReducer from "./reducers/taskReducer";
+
+const store = createStore(taskReducer, applyMiddleware(thunk));
+
+export default store;
